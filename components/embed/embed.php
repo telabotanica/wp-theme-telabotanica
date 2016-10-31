d38336523be7eb35bf2431c44e956133d5c7f690
Hans Lemuet <hans@etaminstudio.com>
2016-10-31 20:56:27 +0100

    Composant embed : ajout d'une description (accessibilité et référencement)

--- components/embed/embed.php
+++ components/embed/embed.php
@@ -1,5 +1,9 @@
 <?php function telabotanica_component_embed($data) {
   if (!isset($data->method)) $data->method = get_sub_field('method');
+  if (!isset($data->description)) {
+    $data->description = get_sub_field('description');
+    $data->description_id = get_sub_field_object('description')['name'];
+  }
 
   if ( $data->method === 'oembed' ) :
 
@@ -16,7 +20,13 @@
 
   echo '<div class="component component-embed">';
 
-  echo $data->embed;
+  echo '<div class="component-embed-wrapper" aria-describedby="' . $data->description_id . '">';
+    echo $data->embed;
+  echo '</div>';
+
+  echo '<div class="component-embed-description" id="' . $data->description_id . '">';
+    echo $data->description;
+  echo '</div>';
 
   echo '</div>';
 }

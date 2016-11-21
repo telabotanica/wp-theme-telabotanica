140807fa341ab174174be62a98e8ac7c51b9b446
Hans Lemuet <hans@etaminstudio.com>
2016-11-21 16:15:43 +0100

    Mini corrections dans les options

--- inc/options.php
+++ inc/options.php
@@ -2,15 +2,15 @@
 
 if( function_exists('acf_add_options_page') ) {
 
-  // Options pour les Actualités
+  // Options pour la lettre d'actu
   acf_add_options_page(array(
-    'page_title' 	=> __( "Lettre d'actu", 'californiefrancaise' ),
-    'menu_title'	=> __( "Lettre d'actu", 'californiefrancaise' ),
-    'menu_slug' 	=> 'newsletter',
-    'capability'	=> 'edit_posts',
+    'page_title'  => __( "Lettre d'actu", 'telabotanica' ),
+    'menu_title'  => __( "Lettre d'actu", 'telabotanica' ),
+    'menu_slug'   => 'newsletter',
+    'capability'  => 'edit_posts',
     'position'    => 30.3,
     'icon_url'    => 'dashicons-email-alt',
-    'redirect'		=> false
+    'redirect'    => false
 	));
 
 }

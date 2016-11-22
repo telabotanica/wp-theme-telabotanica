b40b09ff6f8e38439b86e79acba18b07be3a381b
Mathias Chouet <mathias@tela-botanica.org>
2016-11-22 11:09:41 +0100

    Suite intégration eFlore
    Remplacement du <head> en tas pas des fonctions WP de gestion des scripts et styles
    Page de config ACF "Applis externes" pour régler les chemins d'eFlore et Chorologie

--- inc/options.php
+++ inc/options.php
@@ -2,15 +2,25 @@
 
 if( function_exists('acf_add_options_page') ) {
 
-  // Options pour les Actualités
-  acf_add_options_page(array(
-    'page_title' 	=> __( "Lettre d'actu", 'californiefrancaise' ),
-    'menu_title'	=> __( "Lettre d'actu", 'californiefrancaise' ),
-    'menu_slug' 	=> 'newsletter',
-    'capability'	=> 'edit_posts',
-    'position'    => 30.3,
-    'icon_url'    => 'dashicons-email-alt',
-    'redirect'		=> false
+	// Options pour les Actualités
+	acf_add_options_page(array(
+		'page_title' 	=> __( "Lettre d'actu", 'californiefrancaise' ),
+		'menu_title'	=> __( "Lettre d'actu", 'californiefrancaise' ),
+		'menu_slug' 	=> 'newsletter',
+		'capability'	=> 'edit_posts',
+		'position'    => 30.3,
+		'icon_url'    => 'dashicons-email-alt',
+		'redirect'		=> false
 	));
 
+	// Options pour les application externes intégrées (eFlore, Chorologie...)
+	acf_add_options_page(array(
+		'page_title' 	=> __( "Applis externes", 'tb_applis_externes' ),
+		'menu_title'	=> __( "Applis externes", 'tb_applis_externes' ),
+		'menu_slug' 	=> 'tb_applis_externes',
+		'capability'	=> 'administrator',
+		'position'    => 105,
+		'icon_url'    => 'dashicons-screenoptions',
+		'redirect'		=> false
+	));
 }

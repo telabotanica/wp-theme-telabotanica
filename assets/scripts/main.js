4151e533a7cab7756a79f8435c0422f62fc1477d
Hans Lemuet <hans@etaminstudio.com>
2016-11-22 11:22:10 +0100

    share : ajout du script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,11 @@ require('../styles/editor-style.scss');
 // Icons
 require('../icons/_all.js');
 
+// Modules
+// Require all script.js files in the modules folder
+var req = require.context('../../modules/', true, /script\.js$/);
+req.keys().forEach(req);
+
 // Composants
 // Require all script.js files in the components folder
 var req = require.context('../../components/', true, /script\.js$/);

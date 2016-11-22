6d9a78507fd83fc6b1a6d3de7ae4c8108cdf70e8
Mathias Chouet <mathias@tela-botanica.org>
2016-11-22 15:19:37 +0100

    Amélioration du chargement d'eFlore

--- template-eflore.php
+++ template-eflore.php
@@ -16,13 +16,16 @@ $chemin_eflore_http = get_field('applis_externes_chemin_eflore_http', 'options')
 // Inclusion de l'appli
 if (is_dir($dossier_eflore)) {
 	$chemin_eflore = $dossier_eflore . "/eflore_wordpress.php";
-	include $chemin_eflore;
+	require $chemin_eflore;
+
+	// Rendu
+	$content_class = [ 'main-content-inner' ];
+	get_header();
+	echo eflore_get_contenu(); // défini dans eflore_wordpress.php
 } else {
+	get_header();
 	echo "Impossible de charger eFlore, vérifiez la configuration.";
 }
 
-// Rendu
-get_header();
-$content_class = [ 'main-content-inner' ];
-echo eflore_get_contenu(); // défini dans eflore_wordpress.php
 get_footer();
+

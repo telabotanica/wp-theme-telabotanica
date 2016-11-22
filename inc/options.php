057fdce7e9ae6b46f81006c9f39bd343ad4e50da
kstefanini <stefanini.k@gmail.com>
2016-11-22 15:38:20 +0100

    Modification de l'organisation du menu newsletter

--- inc/options.php
+++ inc/options.php
@@ -4,12 +4,11 @@ if( function_exists('acf_add_options_page') ) {
 
   // Options pour les Actualités
   acf_add_options_page(array(
-    'page_title' 	=> __( "Lettre d'actu", 'californiefrancaise' ),
-    'menu_title'	=> __( "Lettre d'actu", 'californiefrancaise' ),
-    'menu_slug' 	=> 'newsletter',
+    'page_title' 	=> 'Composer',
+    'menu_title'	=> 'Composer la newsletter',
+    'menu_slug' 	=> 'newsletter_compose',
+    'parent_slug'	=> 'newsletter',
     'capability'	=> 'edit_posts',
-    'position'    => 30.3,
-    'icon_url'    => 'dashicons-email-alt',
     'redirect'		=> false
 	));
 

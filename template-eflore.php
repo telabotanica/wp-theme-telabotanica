3e3dccc805b2362fabecb92a0f3325e7e0041c70
Mathias Chouet <mathias@tela-botanica.org>
2016-11-22 11:09:41 +0100

    Suite intégration eFlore
    Remplacement du <head> en tas pas des fonctions WP de gestion des scripts et styles
    Page de config ACF "Applis externes" pour régler les chemins d'eFlore et Chorologie

--- template-eflore.php
+++ template-eflore.php
@@ -1,25 +1,30 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template pour l'affichage d'eFlore
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package cc2
+ * Amorce le fichier eflore_wordpress.php contenu dans le code
+ * d'eflore-consultation; nécessite de remplir la configuration dans la page
+ * "Applis externes" du Tableau de Bord Wordpress
  */
  /*
 Template Name: eflore
 */
+
 $content_class = [ 'main-content-inner' ];
 
+get_header();
 
-get_header(); ?>
+$dossier_eflore = get_field('applis_externes_chemin_eflore', 'options');
+$chemin_eflore_http = get_field('applis_externes_chemin_eflore_http', 'options');
 
+// Inclusion de l'appli
+if (is_dir($dossier_eflore)) {
+	$chemin_eflore = $dossier_eflore . "/eflore_wordpress.php";
+	include $chemin_eflore;
+} else {
+	echo "Impossible de charger eFlore, vérifiez la configuration.";
+}
 
+get_footer();
 
-						<?php include("../eflore-test/consultation/eflore_wordpress.php"); ?>
-
-
-<?php get_footer(); ?>
+?>

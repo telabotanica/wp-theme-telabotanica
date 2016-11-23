11c65c5f627f10b7ff51e2c713ea4ceb03522c72
Mathias Chouet <mathias@tela-botanica.org>
2016-11-23 16:54:55 +0100

    Template d'intégration de l'appli Chorologie

---
+++ template-chorologie.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template pour l'affichage de Chorologie
+ *
+ * Amorce le fichier chorologie_wordpress.php contenu dans le code
+ * de Chorologie; nécessite de remplir la configuration dans la page
+ * "Applis externes" du Tableau de Bord Wordpress
+ */
+ /*
+Template Name: chorologie
+*/
+
+$dossier_chorologie = get_field('applis_externes_chemin_chorologie', 'options');
+$chemin_chorologie_http = get_field('applis_externes_chemin_chorologie_http', 'options');
+
+// Inclusion de l'appli
+if (is_dir($dossier_chorologie)) {
+	$chemin_chorologie = $dossier_chorologie . "/chorologie_wordpress.php";
+	require $chemin_chorologie;
+
+	// Rendu
+	get_header();
+	// fonctions définies dans chorologie_wordpress.php
+	//echo chorologie_get_contenu_tete();
+	//echo chorologie_get_contenu_navigation();
+	echo chorologie_get_contenu();
+	echo chorologie_get_contenu_pied();
+} else {
+	get_header();
+	echo "Impossible de charger Chorologie, vérifiez la configuration.";
+}
+
+get_footer();

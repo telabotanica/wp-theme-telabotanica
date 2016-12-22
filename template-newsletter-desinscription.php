ff03dbf8a4d2c9d58ef27cf2a9113cd5e290c40a
Mathias Chouet <mathias@tela-botanica.org>
2016-12-22 14:34:12 +0100

    Amélioration page de désinscription newsletter
    Style plus harmonieux
    Gestion d'erreurs et envoi d'emails d'alerte en cas d'erreur

--- template-newsletter-desinscription.php
+++ template-newsletter-desinscription.php
@@ -10,9 +10,22 @@
 Template Name: newsletter-desinscription
 */
 
-// Entête du thème
 get_header();
 
+?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php //the_telabotanica_module('cover'); ?>
+		<div class="layout-wrapper">
+			<div class="layout-content">
+				<?php the_telabotanica_module('breadcrumbs'); ?>
+				<article class="article">
+					<div class="component component-title level-2">
+						<h1>Me désinscrire de la lettre d'actualités</h1>
+					</div>
+
+<?php
 // Détection du plugin Tela Botanica
 // @WARNING attention à ne pas renommer le dossier
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
@@ -32,6 +45,7 @@ if (is_plugin_active( 'tela-botanica/tela-botanica.php')) {
 			$ezmlm_php_header = $newsletter_config['ezmlm_php_header'];
 		}
 		$adresse_liste = $newsletter_config['newsletter_recipient'];
+		$destinataires_emails_erreurs = $newsletter_config['error_recipients_emails'];
 		// ezmlm-php s'attend à recevoir le nom de la liste, sans son domaine
 		$nom_liste = substr($adresse_liste, 0, strpos($adresse_liste, '@'));
 
@@ -65,21 +79,55 @@ if (is_plugin_active( 'tela-botanica/tela-botanica.php')) {
 				</p>
 				<?php
 			} else {
-				var_dump($contenu);
-				var_dump($http_code);
-				var_dump(curl_error($ch));
+				/**
+				 * Lors d'un échec de désinscription, envoie une alerte par email
+				 * aux destinataires listés dans la configuration du plugin :
+				 * "Newsletter" => "Réglages" => "Destinataires des emails d'erreurs"
+				 */
+				$message = 'Désinscription à la lettre d\'actu : erreur ! ' . "\r\n"
+					. "\r\n"
+					. 'Adresse à désinscrire : ' . $email . "\r\n"
+					. 'Liste : ' . $adresse_liste . "\r\n"
+					. 'URL appelée : ' . $unsubscribe_url . "\r\n"
+					. 'Code de retour HTTP : ' . $http_code . "\r\n"
+					. 'Erreur cURL : ' . curl_error($ch) . "\r\n"
+					. 'Message renvoyé par le service : ' . $contenu . "\r\n"
+				;
+
+				$headers = 'Content-Type: text/plain; charset="utf-8"' . "\r\n"
+					. 'Content-Transfer-Encoding: 8bit' . "\r\n"
+					. 'From: wp-newsletter@tela-botanica.org' . "\r\n"
+					. 'Reply-To: no-reply@example.com' . "\r\n"
+					. 'X-Mailer: PHP/' . phpversion()
+				;
+
+				foreach ($destinataires_emails_erreurs as $destinataire) {
+					if ($destinataire && '#' !== substr($destinataire, 0, 1)) {
+						error_log($message, 1, $destinataire, $headers);
+						error_log($message);
+					}
+				}
+				?>
+				<p>
+					Une erreur est survenue lors de la désinscription, nous en avons eté informés.
+					<br/>
+					Pour plus d'informations n'hésitez pas à nous contacter.
+				</p>
+				<?php
 			}
 
 			curl_close($ch);
 		}
 		?>
-		<form method="post" action="">
+		<form method="post" action="" class="form-newsletter layout-column">
 			<input type="hidden" name="name" id="name">
-
-			<label for="email">Votre adresse email</label>
-			<input type="email" name="email" id="email" size="60">
-
-			<input type="submit" name="submit" value="Me désinscrire">
+			<fieldset class="form-newsletter-fields">
+				<input class="form-newsletter-email" type="email" name="email" placeholder="Votre adresse e-mail" type="email">
+				<svg aria-hidden="true" role="img" class="icon icon-mail "><use xlink:href="#icon-mail"/></svg>
+				<button class="form-newsletter-button" type="submit">
+					Me désinscrire
+				</button>
+			</fieldset>
 		</form>
 		<?php
 	} else {
@@ -104,5 +152,12 @@ if (is_plugin_active( 'tela-botanica/tela-botanica.php')) {
 	<?php
 }
 
-// Pied du thème
+?>
+				</article>
+			</div>
+		</div>
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php
 get_footer();

d7be8fa691a750d256d04f7bfb03c5f40c5795ae
Mathias Chouet <mathias@tela-botanica.org>
2017-01-05 17:49:17 +0100

    Module share: fermeture des liens

--- modules/share/share.php
+++ modules/share/share.php
@@ -8,19 +8,19 @@
 
   echo '<ul class="share">';
     echo sprintf(
-      '<li class="share-item"><a href="%s" target="_blank" title="%s">%s</li>',
+      '<li class="share-item"><a href="%s" target="_blank" title="%s">%s</a></li>',
       'https://www.facebook.com/sharer/sharer.php?u=' . urlencode( $url ),
       sprintf( __( 'Partager sur %s', 'telabotanica' ), 'Facebook' ),
       get_telabotanica_module('icon', ['icon' => 'facebook'])
     );
     echo sprintf(
-      '<li class="share-item"><a href="%s" target="_blank" title="%s">%s</li>',
+      '<li class="share-item"><a href="%s" target="_blank" title="%s">%s</a></li>',
       'https://twitter.com/intent/tweet?text=' . urlencode( get_the_title() . ' ' . $url ),
       sprintf( __( 'Partager sur %s', 'telabotanica' ), 'Twitter' ),
       get_telabotanica_module('icon', ['icon' => 'twitter'])
     );
     echo sprintf(
-      '<li class="share-item"><a href="%s" target="_blank" title="%s">%s</li>',
+      '<li class="share-item"><a href="%s" target="_blank" title="%s">%s</a></li>',
       'mailto:?subject=' . urlencode( get_the_title() ) . '&body=' . urlencode( $url ),
       __( 'Partager par courriel', 'telabotanica' ),
       get_telabotanica_module('icon', ['icon' => 'mail'])

d1ec4ed98773124038722bdee0097594da72531c
Mathias Chouet <mathias@tela-botanica.org>
2017-01-18 17:25:02 +0100

    Template conditionnel pour les pages outils
    nouveau template centré large
    début du style de l'outil forum

---
+++ buddypress/groups/single/ep-centre-large.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * BuddyPress - Group - Large centered content
+ *
+ * @package BuddyPress
+ * @subpackage bp-legacy
+ */
+?>
+
+<div class="layout-central-col">
+	<div class="layout-wrapper">
+		<div class="layout-content-large">
+			<?php			
+			/**
+			 * Fires before the display of content for plugins using the BP_Group_Extension.
+			 *
+			 * @since 1.2.0
+			 */
+			do_action( 'bp_before_group_plugin_template' ); ?>
+
+			<?php
+			/**
+			 * Fires and displays content for plugins using the BP_Group_Extension.
+			 *
+			 * @since 1.0.0
+			 */
+			do_action( 'bp_template_content' ); ?>
+
+			<?php
+			/**
+			 * Fires after the display of content for plugins using the BP_Group_Extension.
+			 *
+			 * @since 1.2.0
+			 */
+			do_action( 'bp_after_group_plugin_template' );
+			?>
+		</div>
+	</div>
+</div>

779a05f7c17f8c293d6592358993eed6789494b0
Mathias Chouet <mathias@tela-botanica.org>
2017-01-24 11:21:30 +0100

    Projets / forum : confirmation avant (dés)abonnement

--- modules/project-forum/script.js
+++ modules/project-forum/script.js
@@ -2,68 +2,34 @@
 
 var Tela = window.Tela || {};
 
-Tela.epFloraData = (function(){
+Tela.epForum = (function(){
 
-  function manageTabs(menuSelector, bodySelector){
-    var $zoneMenu = $(menuSelector),
-        $zoneOutils = $(bodySelector),
-        $tabs,
-        $entreesListe,
-        $outils;
+  function manageForum(){
+    var $formAbonnement = $('#tb-forum-inscription'),
+        $boutonAbonnement = $formAbonnement.find('input[type="submit"]');
 
     function init(){
-      $tabs = $zoneMenu.find('a.toc-item-link');
-      $entreesListe = $zoneMenu.find('li.toc-item');
-      $outils = $zoneOutils.find('.ep-flora-data-tab');
-
-      manageMenu();
-
-      $tabs.on('click', changeTab);
+      $boutonAbonnement.on('click', confirmFormSubmit);
     }
 
-    function manageMenu() {
-      // supprimer les entrées de menu correspondant aux outils non activés
-      $tabs.each(function() {
-        var idOutil = $(this).attr('href'),
-            outilPointe = $(idOutil);
-        if (! outilPointe.length) {
-            $(this).closest('li').remove();
-        }
-      });
-      // activer l'entrée de menu correspondant à l'outil actuellement visible
-      if ($outils.length) {
-        var outilVisible = $outils.first(),
-          idOutilVisible = outilVisible.attr('id'),
-          lienMenu = $tabs.filter('[href="#' + idOutilVisible + '"]');
-        lienMenu.closest('li').addClass('is-active');
-      }
-    }
-
-    function changeTab(e){
-      //console.log('ça clique sa mémé');
+    function confirmFormSubmit(e){
       e.preventDefault();
       e.stopPropagation();
-      // active le bon lien dans le menu
-      $entreesListe.removeClass('is-active'); // bruteforce
-      $(this).closest('li').addClass('is-active');
-      // rend visible le bon outil Flora Data
-      var idOutil = $(this).attr('href'),
-          outilChoisi = $(idOutil);
-      $outils.each(function() {
-        $(this).hide();
-      });
-      outilChoisi.show();
+      var message = $boutonAbonnement.attr("title");
+      if (confirm(message)) {
+        $formAbonnement.submit();
+	  }
     }
 
     init();
   }
 
-  return function(menuSelector, bodySelector){
-    manageTabs(menuSelector, bodySelector);
+  return function(){
+    manageForum();
   };
 
 })();
 
 $(document).ready(function(){
-  Tela.epFloraData('#ep-flora-data-menu', '#ep-flora-data-tabs');
+  Tela.epForum();
 });

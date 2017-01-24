13a653ac7cce4401eee29b317bde3ac3f9ca69b3
Mathias Chouet <mathias@tela-botanica.org>
2017-01-24 15:47:29 +0100

    Style des pages Projet : suite
    page d'accueil archiminimale
    page des membres

---
+++ buddypress/groups/single/front.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * BuddyPress - Group - Custom front page for Tela Botanica
+ *
+ * @package BuddyPress
+ * @subpackage bp-legacy
+ */
+?>
+
+<div class="layout-central-col">
+	<div class="layout-wrapper">
+		<div class="layout-content">
+			<?php
+			$group = groups_get_current_group();
+			//var_dump($group);
+			
+			$descriptionAAfficher = $group->description;
+			$descriptionComplete = groups_get_groupmeta($group->id, 'description-complete');
+			//var_dump($descriptionComplete);
+			if ($descriptionComplete) {
+				$descriptionAAfficher = $descriptionComplete;
+			}
+			?>
+			<div class="project-front-description"><?php _e("", 'telabotanica') ?>
+				<?php echo $descriptionAAfficher; ?>
+			</div>
+		</div>
+	</div>
+</div>
--- buddypress/groups/single/home.php
+++ buddypress/groups/single/home.php
@@ -20,122 +20,116 @@ the_telabotanica_module('cover-project', []);
 the_telabotanica_module('nav-project', []);
 ?>
 
-<!--<div class="layout-central-col">
-  <div class="layout-wrapper">
-    <div class="layout-content" id="buddypress">-->
+<div id="item-body">
 
-    	<div id="item-body">
+	<?php
 
-    		<?php
+	/**
+	 * Fires before the display of the group home body.
+	 *
+	 * @since 1.2.0
+	 */
+	do_action( 'bp_before_group_body' );
 
-    		/**
-    		 * Fires before the display of the group home body.
-    		 *
-    		 * @since 1.2.0
-    		 */
-    		do_action( 'bp_before_group_body' );
+	/**
+	 * Does this next bit look familiar? If not, go check out WordPress's
+	 * /wp-includes/template-loader.php file.
+	 *
+	 * @todo A real template hierarchy? Gasp!
+	 */
 
-    		/**
-    		 * Does this next bit look familiar? If not, go check out WordPress's
-    		 * /wp-includes/template-loader.php file.
-    		 *
-    		 * @todo A real template hierarchy? Gasp!
-    		 */
+		// Looking at home location
+		if ( bp_is_group_home() ) :
 
-    			// Looking at home location
-    			if ( bp_is_group_home() ) :
+			if ( bp_group_is_visible() ) {
 
-    				if ( bp_group_is_visible() ) {
+				// Load appropriate front template
+				bp_groups_front_template_part();
 
-    					// Load appropriate front template
-    					bp_groups_front_template_part();
+			} else {
 
-    				} else {
+				/**
+				 * Fires before the display of the group status message.
+				 *
+				 * @since 1.1.0
+				 */
+				do_action( 'bp_before_group_status_message' ); ?>
 
-    					/**
-    					 * Fires before the display of the group status message.
-    					 *
-    					 * @since 1.1.0
-    					 */
-    					do_action( 'bp_before_group_status_message' ); ?>
+				<div id="message" class="info">
+					<p><?php bp_group_status_message(); ?></p>
+				</div>
 
-    					<div id="message" class="info">
-    						<p><?php bp_group_status_message(); ?></p>
-    					</div>
+				<?php
 
-    					<?php
+				/**
+				 * Fires after the display of the group status message.
+				 *
+				 * @since 1.1.0
+				 */
+				do_action( 'bp_after_group_status_message' );
 
-    					/**
-    					 * Fires after the display of the group status message.
-    					 *
-    					 * @since 1.1.0
-    					 */
-    					do_action( 'bp_after_group_status_message' );
+			}
 
-    				}
+		// Not looking at home
+		else :
 
-    			// Not looking at home
-    			else :
+			// Group Admin
+			if     ( bp_is_group_admin_page() ) : bp_get_template_part( 'groups/single/admin'        );
 
-    				// Group Admin
-    				if     ( bp_is_group_admin_page() ) : bp_get_template_part( 'groups/single/admin'        );
+			// Group Activity
+			elseif ( bp_is_group_activity()   ) : bp_get_template_part( 'groups/single/activity'     );
 
-    				// Group Activity
-    				elseif ( bp_is_group_activity()   ) : bp_get_template_part( 'groups/single/activity'     );
+			// Group Members
+			// remplace bp_groups_members_template_part() qui impose de placer la recherche / le
+			// filtrage des membres hors du template (pas pratique)
+			elseif ( bp_is_group_members()    ) : bp_get_template_part( 'groups/single/members'     );
 
-    				// Group Members
-    				elseif ( bp_is_group_members()    ) : bp_groups_members_template_part();
+			// Group Invitations
+			elseif ( bp_is_group_invites()    ) : bp_get_template_part( 'groups/single/send-invites' );
 
-    				// Group Invitations
-    				elseif ( bp_is_group_invites()    ) : bp_get_template_part( 'groups/single/send-invites' );
+			// Old group forums
+			elseif ( bp_is_group_forum()      ) : bp_get_template_part( 'groups/single/forum'        );
 
-    				// Old group forums
-    				elseif ( bp_is_group_forum()      ) : bp_get_template_part( 'groups/single/forum'        );
+			// Membership request
+			elseif ( bp_is_group_membership_request() ) : bp_get_template_part( 'groups/single/request-membership' );
 
-    				// Membership request
-    				elseif ( bp_is_group_membership_request() ) : bp_get_template_part( 'groups/single/request-membership' );
+			// Anything else (plugins mostly)
+			else :
+				$current_action = bp_current_action();
+				//var_dump($current_action);
+				switch ($current_action) :
+					case 'forum':
+					case 'porte-documents':
+					case 'wiki':
+						bp_get_template_part('groups/single/ep-centre-large');
+						break;
+					case 'flora-data':
+						bp_get_template_part('groups/single/ep-flora-data');
+						break;
+					default:
+						bp_get_template_part('groups/single/plugins');
+				endswitch;
 
-    				// Anything else (plugins mostly)
-    				else :
-						$current_action = bp_current_action();
-						//var_dump($current_action);
-						switch ($current_action) :
-							case 'forum':
-							case 'porte-documents':
-							case 'wiki':
-								bp_get_template_part('groups/single/ep-centre-large');
-								break;
-							case 'flora-data':
-								bp_get_template_part('groups/single/ep-flora-data');
-								break;
-							default:
-								bp_get_template_part('groups/single/plugins');
-						endswitch;
+			endif;
 
-    				endif;
+		endif;
 
-    			endif;
+	/**
+	 * Fires after the display of the group home body.
+	 *
+	 * @since 1.2.0
+	 */
+	do_action( 'bp_after_group_body' ); ?>
 
-    		/**
-    		 * Fires after the display of the group home body.
-    		 *
-    		 * @since 1.2.0
-    		 */
-    		do_action( 'bp_after_group_body' ); ?>
+</div><!-- #item-body -->
 
-    	</div><!-- #item-body -->
+<?php
 
-    	<?php
+/**
+ * Fires after the display of the group home content.
+ *
+ * @since 1.2.0
+ */
+do_action( 'bp_after_group_home_content' ); ?>
 
-    	/**
-    	 * Fires after the display of the group home content.
-    	 *
-    	 * @since 1.2.0
-    	 */
-    	do_action( 'bp_after_group_home_content' ); ?>
-
-    	<?php endwhile; endif; ?>
-
-    <!-- #buddypress --><!--</div>
-  </div>
-</div>-->
+<?php endwhile; endif; ?>

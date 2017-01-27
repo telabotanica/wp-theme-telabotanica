c44dec731dafd6919a1e879e41f19657d1a17f8c
Hans Lemuet <hans@etaminstudio.com>
2017-01-27 17:33:33 +0100

    cover-home: stats style

--- modules/cover-home/cover-home.php
+++ modules/cover-home/cover-home.php
@@ -1,11 +1,11 @@
 <?php function telabotanica_module_cover_home($data) {
   if (!isset($data->image)) $data->image = get_field('cover_image');
   if (!isset($data->title)) $data->title = __('Bienvenue sur Tela Botanica, <br />le réseau des botanistes francophones', 'telabotanica');
-  if (!isset($data->content)) $data->content = false;
   if (!isset($data->modifiers)) $data->modifiers = '';
   ?>
   <div class="cover-home <?php echo $data->modifiers ?>" style="background-image: url(<?php echo $data->image['url'] ?>);">
     <div class="layout-wrapper">
+      <div class="cover-home-content">
       <?php
       if ( is_user_logged_in() ) :
         $current_user = wp_get_current_user();
@@ -24,18 +24,37 @@
           $data->title
         );
       endif;
+      ?>
+      </div>
 
-      if ($data->content) :
-        echo sprintf(
-          '<div class="cover-home-content">%s</div>',
-          $data->content
-        );
-      endif; ?>
-
+      <?php
+      // TODO: brancher les compteurs et les liens
+      $users_link = '#';
+      $user_count = 10000;
+      $observations_link = '#';
+      $observations_count = 10000;
+      $get_involved_link = '#';
+      $get_involved_count = wp_count_posts('tb_participer')->publish;
+      ?>
       <ul class="cover-home-stats">
-        <li class="cover-home-stats-item cover-home-stats-item-users"><a href="#"><var>XX XXX</var> télabotanistes</a></li>
-        <li class="cover-home-stats-item cover-home-stats-item-observations"><a href="#"><var>XX XXX</var> observations</a></li>
-        <li class="cover-home-stats-item cover-home-stats-item-get-involved"><a href="#"><var>XX XXX</var> façons de participer</a></li>
+        <li class="cover-home-stats-item cover-home-stats-item-users">
+          <a href="<?php echo $users_link ?>">
+            <div class="cover-home-stats-icon"><?php the_telabotanica_module('icon', ['icon' => 'mail']) ?></div>
+            <?php printf(__('%s télabotanistes', 'telabotanica'), '<var>' . number_format_i18n($user_count) . '</var>') ?>
+          </a>
+        </li>
+        <li class="cover-home-stats-item cover-home-stats-item-observations">
+          <a href="<?php echo $observations_link ?>">
+            <div class="cover-home-stats-icon"><?php the_telabotanica_module('icon', ['icon' => 'mail']) ?></div>
+            <?php printf(__('%s observations', 'telabotanica'), '<var>' . number_format_i18n($observations_count) . '</var>') ?>
+          </a>
+        </li>
+        <li class="cover-home-stats-item cover-home-stats-item-get-involved">
+          <a href="<?php echo $get_involved_link ?>">
+            <div class="cover-home-stats-icon"><?php the_telabotanica_module('icon', ['icon' => 'mail']) ?></div>
+            <?php printf(__('%s façons de participer', 'telabotanica'), '<var>' . number_format_i18n($get_involved_count) . '</var>') ?>
+          </a>
+        </li>
       </ul>
     </div>
     <?php

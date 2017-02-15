aa0405efea766468e280ff10861a129fdba7cd90
Hans Lemuet <hans@etaminstudio.com>
2017-02-15 21:11:28 +0100

    spaces > tabs

--- webpack.config.js
+++ webpack.config.js
@@ -10,98 +10,98 @@ const extractEditorStyle = new ExtractTextPlugin('editor-style.css');
 const extractLoginStyle = new ExtractTextPlugin('login-style.css');
 
 module.exports = {
-  entry: './assets/scripts/main.js',
-  output: {
-    path: './dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /main\.scss$/,
-        loader: extractBundle.extract({
-          fallbackLoader: 'style-loader',
-          loader: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
-        })
-      },
-      {
-        test: /editor-style\.scss$/,
-        loader: extractEditorStyle.extract({
-          fallbackLoader: 'style-loader',
-          loader: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
-        })
-      },
-      {
-        test: /login-style\.scss$/,
-        loader: extractLoginStyle.extract({
-          fallbackLoader: 'style-loader',
-          loader: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
-        })
-      },
-      {
-        test: /\.pug$/,
+	entry: './assets/scripts/main.js',
+	output: {
+		path: './dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /main\.scss$/,
+				loader: extractBundle.extract({
+					fallbackLoader: 'style-loader',
+					loader: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
+				})
+			},
+			{
+				test: /editor-style\.scss$/,
+				loader: extractEditorStyle.extract({
+					fallbackLoader: 'style-loader',
+					loader: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
+				})
+			},
+			{
+				test: /login-style\.scss$/,
+				loader: extractLoginStyle.extract({
+					fallbackLoader: 'style-loader',
+					loader: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
+				})
+			},
+			{
+				test: /\.pug$/,
 				include: [
-          path.resolve(__dirname, "modules")
-        ],
-        loader: 'pug-loader'
-      },
-      {
-        test: /\.svg$/,
-        include: [
-          path.resolve(__dirname, "assets/icons")
-        ],
-        loader: 'svg-sprite-loader?name=icon-[name]'
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        include: [
-          path.resolve(__dirname, "assets/fonts")
-        ],
-        loader: 'file-loader?name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.svg$/,
-        exclude: [
-          path.resolve(__dirname, "assets/fonts"),
-          path.resolve(__dirname, "assets/icons")
-        ],
-        loader: 'svg-url-loader'
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/i,
-        exclude: [
-          path.resolve(__dirname, "assets/fonts"),
-          path.resolve(__dirname, "assets/icons")
-        ],
-        loader: 'url-loader?limit=10000'
-      },
-    ]
-  },
-  plugins: [
-    extractBundle,
-    extractEditorStyle,
-    extractLoginStyle,
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      postcss: [
-        autoprefixer({
-          browsers: [
-            'last 5 versions',
-            'android 4',
-            'opera 12',
-          ],
-        }),
-      ],
-    }),
-    // Make $ and jQuery available in every module without writing require("jquery")
-    new webpack.ProvidePlugin({
-      $: "jquery"
-    }),
-  ],
-  externals: {
-    // require("jquery") is external and available
-    //  on the global var jQuery
-    "jquery": "jQuery"
-  }
+					path.resolve(__dirname, "modules")
+				],
+				loader: 'pug-loader'
+			},
+			{
+				test: /\.svg$/,
+				include: [
+					path.resolve(__dirname, "assets/icons")
+				],
+				loader: 'svg-sprite-loader?name=icon-[name]'
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				include: [
+					path.resolve(__dirname, "assets/fonts")
+				],
+				loader: 'file-loader?name=fonts/[name].[ext]'
+			},
+			{
+				test: /\.svg$/,
+				exclude: [
+					path.resolve(__dirname, "assets/fonts"),
+					path.resolve(__dirname, "assets/icons")
+				],
+				loader: 'svg-url-loader'
+			},
+			{
+				test: /\.(jpe?g|png|gif)$/i,
+				exclude: [
+					path.resolve(__dirname, "assets/fonts"),
+					path.resolve(__dirname, "assets/icons")
+				],
+				loader: 'url-loader?limit=10000'
+			},
+		]
+	},
+	plugins: [
+		extractBundle,
+		extractEditorStyle,
+		extractLoginStyle,
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true,
+			postcss: [
+				autoprefixer({
+					browsers: [
+						'last 5 versions',
+						'android 4',
+						'opera 12',
+					],
+				}),
+			],
+		}),
+		// Make $ and jQuery available in every module without writing require("jquery")
+		new webpack.ProvidePlugin({
+			$: "jquery"
+		}),
+	],
+	externals: {
+		// require("jquery") is external and available
+		//	on the global var jQuery
+		"jquery": "jQuery"
+	}
 }

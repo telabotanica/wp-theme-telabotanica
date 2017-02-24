ca3796654a62254fb35ed542cf303a5f38390fef
Hans Lemuet <hans@etaminstudio.com>
2017-02-24 12:02:33 +0100

    Styleguide: added useful functions for managing data and modifiers

--- inc/styleguide.php
+++ inc/styleguide.php
@@ -4,8 +4,8 @@
  *
  * Ces fonctions permettent de charger les modules, les blocs et les composants du styleguide.
  * Elles suivent les conventions de nommage de wordpress:
- *    `the_*` pour les fonctions qui affichent un élément
- *    `get_*` pour les fonctions qui retournent un élément
+ *		`the_*` pour les fonctions qui affichent un élément
+ *		`get_*` pour les fonctions qui retournent un élément
  *
  * Elles utilisent également l'espace de nommage `telabotanica` pour éviter
  * les collisions.
@@ -15,54 +15,55 @@
  */
 
  /**
-  * Liste de tous les modules accessibles depuis les fonctions du styleguide
-  */
+	* Liste de tous les modules accessibles depuis les fonctions du styleguide
+	*/
 $telabotanica_modules = [
-  'article',
-  'autocomplete',
-  'breadcrumbs',
-  'button',
-  'button-top',
-  'card-project',
-  'categories',
-  'categories-labels',
-  'cover',
-  'cover-home',
-  'cover-project',
-  'cover-search',
-  'footer',
-  'form-newsletter',
-  'header',
-  'icon',
-  'list-articles',
-  'list-projects',
-  'meta-news',
-  'nav-project',
-  'pagination',
-  'search-box',
-  'search-filters',
-  'search-results',
-  'share',
-  'toc',
-  'upcoming-events',
+	'article',
+	'autocomplete',
+	'breadcrumbs',
+	'button',
+	'button-top',
+	'card-project',
+	'categories',
+	'categories-labels',
+	'cover',
+	'cover-home',
+	'cover-project',
+	'cover-search',
+	'footer',
+	'form-newsletter',
+	'header',
+	'icon',
+	'list-articles',
+	'list-projects',
+	'meta-news',
+	'nav-project',
+	'pagination',
+	'search-box',
+	'search-filters',
+	'search-results',
+	'share',
+	'title',
+	'toc',
+	'upcoming-events',
 ];
 array_walk($telabotanica_modules, function ($module) {
-  if (!locate_template('modules/' . $module . '/' . $module . '.php', true, true)) {
-    trigger_error(sprintf(__('Erreur lors de la recherche de %s pour inclusion', 'telabotanica'), $module), E_USER_ERROR);
-  }
+	if (!locate_template('modules/' . $module . '/' . $module . '.php', true, true)) {
+		trigger_error(sprintf(__('Erreur lors de la recherche de %s pour inclusion', 'telabotanica'), $module), E_USER_ERROR);
+	}
 });
 
 /**
  * Liste de tous les composants accessibles depuis les fonctions du styleguide
  */
 $telabotanica_blocks = [
-  'focus',
-  'list-features',
-  'main-features',
+	'focus',
+	'list-features',
+	'main-features',
 ];
 array_walk($telabotanica_blocks, function ($block) {
  if (!locate_template('blocks/' . $block . '/' . $block . '.php', true, true)) {
-   trigger_error(sprintf(__('Erreur lors de la recherche de %s pour inclusion', 'telabotanica'), $block), E_USER_ERROR);
+	 trigger_error(sprintf(__('Erreur lors de la recherche de %s pour inclusion', 'telabotanica'), $block), E_USER_ERROR);
  }
 });
 
@@ -70,20 +71,20 @@ array_walk($telabotanica_blocks, function ($block) {
  * Liste de tous les composants accessibles depuis les fonctions du styleguide
  */
 $telabotanica_components = [
-  'accordion',
-  'buttons',
-  'contact',
-  'embed',
-  'image',
-  'intro',
-  'links',
-  'tools',
-  'text',
-  'title',
+	'accordion',
+	'buttons',
+	'contact',
+	'embed',
+	'image',
+	'intro',
+	'links',
+	'tools',
+	'text',
+	'title',
 ];
 array_walk($telabotanica_components, function ($component) {
  if (!locate_template('components/' . $component . '/' . $component . '.php', true, true)) {
-   trigger_error(sprintf(__('Erreur lors de la recherche de %s pour inclusion', 'telabotanica'), $component), E_USER_ERROR);
+	 trigger_error(sprintf(__('Erreur lors de la recherche de %s pour inclusion', 'telabotanica'), $component), E_USER_ERROR);
  }
 });
 
@@ -93,7 +94,7 @@ array_walk($telabotanica_components, function ($component) {
  * @param mixed[] $data Données utilisées par le module.
  */
 function the_telabotanica_module($module, $data = []) {
-  the_telabotanica_styleguide_element('module', $module, $data);
+	the_telabotanica_styleguide_element('module', $module, $data);
 }
 
 /**
@@ -103,7 +104,7 @@ function the_telabotanica_module($module, $data = []) {
  * @return string Le code HTML du module.
  */
 function get_telabotanica_module($module, $data = []) {
-  return get_telabotanica_styleguide_element('module', $module, $data);
+	return get_telabotanica_styleguide_element('module', $module, $data);
 }
 
 /**
@@ -112,7 +113,7 @@ function get_telabotanica_module($module, $data = []) {
  * @param mixed[] $data Données utilisées par le bloc.
  */
 function the_telabotanica_block($block, $data = []) {
-  the_telabotanica_styleguide_element('block', $block, $data);
+	the_telabotanica_styleguide_element('block', $block, $data);
 }
 
 /**
@@ -122,7 +123,7 @@ function the_telabotanica_block($block, $data = []) {
  * @return string Le code HTML du bloc.
  */
 function get_telabotanica_block($block, $data = []) {
-  return get_telabotanica_styleguide_element('block', $block, $data);
+	return get_telabotanica_styleguide_element('block', $block, $data);
 }
 
 /**
@@ -131,7 +132,7 @@ function get_telabotanica_block($block, $data = []) {
  * @param mixed[] $data Données utilisées par le composant.
  */
 function the_telabotanica_component($component, $data = []) {
-  the_telabotanica_styleguide_element('component', $component, $data);
+	the_telabotanica_styleguide_element('component', $component, $data);
 }
 
 /**
@@ -141,7 +142,7 @@ function the_telabotanica_component($component, $data = []) {
  * @return string Le code HTML du composant.
  */
 function get_telabotanica_component($component, $data = []) {
-  return get_telabotanica_styleguide_element('component', $component, $data);
+	return get_telabotanica_styleguide_element('component', $component, $data);
 }
 
 /**
@@ -151,13 +152,13 @@ function get_telabotanica_component($component, $data = []) {
  * @param mixed[] $data Données utilisées par l'élément.
  */
 function the_telabotanica_styleguide_element($type, $name, $data) {
-  $function = 'telabotanica_' . $type . '_' . str_replace('-', '_', $name);
-  if (function_exists($function)) {
-    $data = (object) $data;
-    call_user_func($function, $data);
-  } else {
-    trigger_error(sprintf(__('Le %s `%s` n\'existe pas dans le styleguide. Avez-vous pensé à l\'ajouter à la liste des éléments dans inc/styleguide.php ?', 'telabotanica'), $type, $name), E_USER_WARNING);
-  }
+	$function = 'telabotanica_' . $type . '_' . str_replace('-', '_', $name);
+	if (function_exists($function)) {
+		$data = (object) $data;
+		call_user_func($function, $data);
+	} else {
+		trigger_error(sprintf(__('Le %s `%s` n\'existe pas dans le styleguide. Avez-vous pensé à l\'ajouter à la liste des éléments dans inc/styleguide.php ?', 'telabotanica'), $type, $name), E_USER_WARNING);
+	}
 }
 
 /**
@@ -168,9 +169,9 @@ function the_telabotanica_styleguide_element($type, $name, $data) {
  * @return string Le code HTML de l'élément.
  */
 function get_telabotanica_styleguide_element($type, $name, $data) {
-  ob_start();
-  the_telabotanica_styleguide_element($type, $name, $data);
-  return ob_get_clean();
+	ob_start();
+	the_telabotanica_styleguide_element($type, $name, $data);
+	return ob_get_clean();
 }
 
 /**
@@ -179,54 +180,71 @@ function get_telabotanica_styleguide_element($type, $name, $data) {
 
 // Sélection du template styleguide en fonction du paraètre `pagename`
 function telabotanica_styleguide_page_template( $template ) {
-  global $wp_query;
-  if ( $wp_query->query_vars['pagename'] === 'styleguide' ) {
-    $new_template = locate_template('styleguide.php');
-    if ( '' != $new_template ) {
-      return $new_template ;
-    }
-  }
-  return $template;
+	global $wp_query;
+	if ( $wp_query->query_vars['pagename'] === 'styleguide' ) {
+		$new_template = locate_template('styleguide.php');
+		if ( '' != $new_template ) {
+			return $new_template ;
+		}
+	}
+	return $template;
 }
 add_filter( 'template_include', 'telabotanica_styleguide_page_template', 99 );
 
+// Merging des data avec les défauts
+function telabotanica_styleguide_data($defaults, $data) {
+	$data = (object) array_merge((array) $defaults, (array) $data);
+	return $data;
+}
+
+// Interprétation des modifiers
+function telabotanica_styleguide_modifiers_array($default, $modifiers) {
+	if (!is_array($modifiers)) $modifiers = explode(' ', $modifiers);
+	if (!is_array($default)) {
+		array_unshift($modifiers, $default);
+	} else {
+		$modifiers = $default + $modifiers;
+	}
+	return $modifiers;
+}
+
 // Définition des paramètres d'URL
 function telabotanica_styleguide_rewrite_tags() {
-  add_rewrite_tag('%styleguide_type%', '([^&]+)');
-  add_rewrite_tag('%styleguide_nom%', '([^&]+)');
+	add_rewrite_tag('%styleguide_type%', '([^&]+)');
+	add_rewrite_tag('%styleguide_nom%', '([^&]+)');
 }
 add_action('init', 'telabotanica_styleguide_rewrite_tags', 10, 0);
 
 // Définition des règles de réécriture d'URL
 function telabotanica_styleguide_rewrite_rules() {
-  add_rewrite_rule('^styleguide/([^/]*)/([^/]*)/?', 'index.php?pagename=styleguide&styleguide_type=$matches[1]&styleguide_nom=$matches[2]', 'top');
-  add_rewrite_rule('^styleguide/?', 'index.php?pagename=styleguide', 'top');
+	add_rewrite_rule('^styleguide/([^/]*)/([^/]*)/?', 'index.php?pagename=styleguide&styleguide_type=$matches[1]&styleguide_nom=$matches[2]', 'top');
+	add_rewrite_rule('^styleguide/?', 'index.php?pagename=styleguide', 'top');
 }
 add_action('init', 'telabotanica_styleguide_rewrite_rules', 10, 0);
 
 // Vider le cache des routes après leur création
 function telabotanica_styleguide_flush_rules(){
-  $rules = get_option( 'rewrite_rules' );
+	$rules = get_option( 'rewrite_rules' );
 
-  if ( !isset( $rules['^styleguide/?'] ) ) {
-    global $wp_rewrite;
-    $wp_rewrite->flush_rules();
-  }
+	if ( !isset( $rules['^styleguide/?'] ) ) {
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+	}
 }
 add_action( 'wp_loaded', 'telabotanica_styleguide_flush_rules' );
 
 // Définition du <title> des pages
 function telabotanica_styleguide_title( $title ) {
-  global $wp_query;
-  if ( $wp_query->query_vars['pagename'] !== 'styleguide' ) { return $title; }
+	global $wp_query;
+	if ( $wp_query->query_vars['pagename'] !== 'styleguide' ) { return $title; }
 
-  $prefix = '';
-  if ($wp_query->get('styleguide_type') && $wp_query->get('styleguide_nom')) {
-    $type = $wp_query->get('styleguide_type');
-    $name = $wp_query->get('styleguide_nom');
-    $prefix = $type . ' ' . $name . ' &#8211; ';
-  }
+	$prefix = '';
+	if ($wp_query->get('styleguide_type') && $wp_query->get('styleguide_nom')) {
+		$type = $wp_query->get('styleguide_type');
+		$name = $wp_query->get('styleguide_nom');
+		$prefix = $type . ' ' . $name . ' &#8211; ';
+	}
 
-  return $prefix . 'Styleguide' . ' &#8211; ' . get_bloginfo('name');
+	return $prefix . 'Styleguide' . ' &#8211; ' . get_bloginfo('name');
 }
 add_filter( 'pre_get_document_title', 'telabotanica_styleguide_title', 10, 2 );

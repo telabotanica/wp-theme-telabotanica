fd1be8bfa47319aa06cbcd6e1805f1ebfb8a86f0
Mathias Chouet <mathias@tela-botanica.org>
2017-03-08 14:26:00 +0100

    Formulaire d'inscription : hack JS pour ajouter un target="_blank" aux liens dans les descriptions des champs

--- modules/registration-form/script.js
+++ modules/registration-form/script.js
@@ -11,9 +11,16 @@ Tela.register = (function(){
     $('#profile-edit-form input:not(:submit), #profile-edit-form textarea, #profile-edit-form select, #signup_form input:not(:submit), #signup_form textarea, #signup_form select').unbind('change');
   }
 
+  function changeLinksTargets() {
+    // adds target="_blank" to links in fields descriptions, which is not
+    // allowed by the fields editor
+    $('p.description > a').prop('target', '_blank');
+  }
+
   return function(selector){
     $(selector).each(function() {
       unbind();
+      changeLinksTargets();
     });
   };
 

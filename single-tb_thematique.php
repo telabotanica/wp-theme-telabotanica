2ad6609a9f54c2e75a3b22c236e9b980674ef4b4
Hans Lemuet <hans@etaminstudio.com>
2017-03-27 15:36:54 +0200

    Added template single-tb_thematique

--- single-tb_thematique.php
+++ single-tb_thematique.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Page
+ * Thématique
  */
 
 get_header(); ?>
@@ -8,15 +8,35 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      <?php the_telabotanica_module('cover', []); ?>
+      <?php the_telabotanica_module('cover'); ?>
 
       <div class="layout-content-col">
         <div class="layout-wrapper">
           <aside class="layout-column">
+            <?php the_telabotanica_module('toc'); ?>
             <?php the_telabotanica_module('button-top'); ?>
           </aside>
           <div class="layout-content">
-            <p>TODO</p>
+            <?php the_telabotanica_module('breadcrumbs'); ?>
+            <article>
+              <?php
+              // Si la page utilise des composants
+              if( have_rows('components') ):
+
+                  // On boucle sur les composants
+                  while ( have_rows('components') ) : the_row();
+
+                    the_telabotanica_component(get_row_layout());
+
+                  endwhile;
+
+              else :
+
+                  // no layouts found
+
+              endif;
+              ?>
+            </article>
           </div>
         </div>
       </div>

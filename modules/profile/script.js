46ccd3b693a9affa8b91a639e46db662dda9ad50
Mathias Chouet <mathias@tela-botanica.org>
2017-03-29 17:21:29 +0200

    Amélioration du formulaire du profil
    autocomplétion sur l'espèce d'intérêt
    restriction des départements au pays "France"

--- modules/profile/script.js
+++ modules/profile/script.js
@@ -10,12 +10,15 @@ Tela.profile = (function(){
 
   function module(selector){
     var $el = $(selector),
-        $searchInput,
-        $currentRequest;
+        $searchInputVille,
+        $searchInputEspece,
+        $currentRequestVille,
+        $currentRequestEspece,
+        $pays;
 
-    function init(){
-      $searchInput = $el.find('.field_ville input[type="text"]');
-      $currentRequest = false;
+    function initAutocompleteVille(){
+      $searchInputVille = $el.find('.field_ville input[type="text"]');
+      $currentRequestVille = false;
 
       /* config */
       var config = {
@@ -27,10 +30,10 @@ Tela.profile = (function(){
       var sources = [
         {
           source: _.debounce(function(query, cb) {
-            if ($currentRequest !== false) {
-                $currentRequest.abort();
+            if ($currentRequestVille !== false) {
+                $currentRequestVille.abort();
             }
-            $currentRequest = $.get(
+            $currentRequestVille = $.get(
               // @TODO parameterize country - tough because we need the ISO code
               'https://api.tela-botanica.org/service:eflore:0.1/osm/zone-admin/?pays=FR&masque=' + query + '%&limite=20',
               function(data) {
@@ -42,10 +45,64 @@ Tela.profile = (function(){
         }
       ];
 
-      algoliaAutocomplete($searchInput[0], config, sources);
+      algoliaAutocomplete($searchInputVille[0], config, sources);
     }
 
-    init();
+    function initAutocompleteEspece() {
+      $searchInputEspece = $el.find('.field_espece-dinteret input[type="text"]');
+      $currentRequestEspece = false;
+
+      /* config */
+      var config = {
+        hint: false,
+        minLength: 3
+      };
+
+      /* setup sources */
+      var sources = [
+        {
+          source: _.debounce(function(query, cb) {
+            if ($currentRequestEspece !== false) {
+                $currentRequestEspece.abort();
+            }
+            $currentRequestEspece = $.get(
+              'http://api.tela-botanica.org/service:eflore:0.1/bdtfx/noms?masque=' + query + '%&retour.format=min&navigation.limite=20&ns.structure=au&retour.tri=alpharet&retour.structure=liste',
+              function(data) {
+                cb(data.resultat);
+              }
+            );
+          }, 300),
+          displayKey: 'nom_sci_complet'
+        }
+      ];
+
+      algoliaAutocomplete($searchInputEspece[0], config, sources);
+    }
+
+    function initRestrictionDepartements() {
+      $pays = $el.find('.field_pays > select');
+      $pays.click(function() {
+        linkPaysDepartement();
+      });
+      linkPaysDepartement();
+    }
+
+    /**
+     * if chosen country is France, enables the "département" selector; else
+     * disables it and clears previously selected "département"
+     */
+    function linkPaysDepartement() {
+      var departement = $el.find('.field_departement > select');
+      var france = $pays.val() == 'France';
+      departement.prop('disabled', !france);
+      if (! france) {
+        departement.val('');
+      }
+    }
+
+    initAutocompleteVille();
+    initAutocompleteEspece();
+    initRestrictionDepartements();
 
     return $el;
   }

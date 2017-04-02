ae36cb7649ac4db245e795c698af0ca6c5524351
Hans Lemuet <hans@etaminstudio.com>
2017-04-02 16:30:01 +0200

    Improved template archive-tb_thematique

--- archive-tb_thematique.php
+++ archive-tb_thematique.php
@@ -8,17 +8,18 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      <?php the_telabotanica_module('cover', [
-        'title' => __('Toutes les thématiques', 'telabotanica'),
-      ] ); ?>
+			<?php the_telabotanica_module('cover', [
+				'title' => __('Toutes les thématiques', 'telabotanica'),
+				'subtitle' => false
+			] ); ?>
 
-      <div class="layout-content-col">
-        <div class="layout-wrapper">
-          <aside class="layout-column">
-            <?php the_telabotanica_module('button-top'); ?>
-          </aside>
-          <div class="layout-content">
-            <?php
+			<div class="layout-content-col">
+				<div class="layout-wrapper">
+					<aside class="layout-column">
+						<?php the_telabotanica_module('button-top'); ?>
+					</aside>
+					<div class="layout-content">
+						<?php
 						$breadcrumbs_items = [ 'home' ];
 
 						$breadcrumbs_items[] = [ 'text' => __('Thématiques', 'telabotanica') ];
@@ -27,30 +28,34 @@ get_header(); ?>
 							'items' => $breadcrumbs_items
 						]);
 
+						// Trier par ordre alphabétique et désactiver la pagination
+						global $query_string;
+						query_posts( $query_string . '&order=ASC&orderby=name&posts_per_page=-1' );
+
 						if ( have_posts() ) : ?>
-              <?php while ( have_posts() ) : the_post(); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                  <?php
-									the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-									the_excerpt();
+							<?php while ( have_posts() ) : the_post(); ?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class('article'); ?>>
+									<?php
+									the_title( sprintf( '<h2 class="article-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+									the_field('cover_subtitle');
 									?>
-                </article>
-              <?php endwhile;
+								</article>
+							<?php endwhile;
 
-              the_posts_pagination( [
-        				'prev_text'          => __( 'Page précédente', 'telabotanica' ),
-        				'next_text'          => __( 'Page suivante', 'telabotanica' ),
-        				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'telabotanica' ) . ' </span>',
-        			] );
-              ?>
-            <?php else :
-              echo '<p>' . __( 'Aucun moyen de participer.', 'telabotanica' ) . '</p>';
-            endif; ?>
-          </div>
-        </div>
-      </div>
+							the_posts_pagination( [
+								'prev_text'          => __( 'Page précédente', 'telabotanica' ),
+								'next_text'          => __( 'Page suivante', 'telabotanica' ),
+								'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'telabotanica' ) . ' </span>',
+							] );
+							?>
+						<?php else :
+							echo '<p>' . __( 'Aucun moyen de participer.', 'telabotanica' ) . '</p>';
+						endif; ?>
+					</div>
+				</div>
+			</div>
 
-    </main><!-- .site-main -->
-  </div><!-- .content-area -->
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
 
 <?php get_footer(); ?>

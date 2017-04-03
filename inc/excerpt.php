18be72550f9ef2104456ed965ddc864092523e43
Hans Lemuet <hans@etaminstudio.com>
2017-04-03 14:02:03 +0200

    Trim buddypress group excerpt to 25 words

--- inc/excerpt.php
+++ inc/excerpt.php
@@ -8,18 +8,25 @@
  */
 function telabotanica_custom_excerpt( $post_excerpt ) {
 
-  // On ajoute l'intro au début de l'excerpt s'il y en a une
-  if ( get_field('intro') ) :
-    $post_excerpt = get_field('intro') . ' ' . $post_excerpt;
-  endif;
+	// On ajoute l'intro au début de l'excerpt s'il y en a une
+	if ( get_field('intro') ) :
+		$post_excerpt = get_field('intro') . ' ' . $post_excerpt;
+	endif;
 
-  // Si l'excerpt est toujours vide, on essaye d'utiliser la description
-  if ( empty( $post_excerpt ) && get_field('description') ) :
-    $post_excerpt = get_field('description');
-  endif;
+	// Si l'excerpt est toujours vide, on essaye d'utiliser la description
+	if ( empty( $post_excerpt ) && get_field('description') ) :
+		$post_excerpt = get_field('description');
+	endif;
 
-  // On recoupe l'excerpt à 20 mots, et on change le suffixe
-  return wp_trim_words( $post_excerpt, 30, '&nbsp;&hellip;' );
+	// On recoupe l'excerpt à 30 mots, et on change le suffixe
+	return wp_trim_words( $post_excerpt, 30, '&nbsp;&hellip;' );
 
 }
 add_filter( 'get_the_excerpt', 'telabotanica_custom_excerpt' );
+
+// Raccourcir l'excerpt des descriptions de groupes BuddyPress
+function telabotanica_group_description_excerpt( $excerpt ) {
+	// On recoupe l'excerpt à 20 mots, et on change le suffixe
+	return wp_trim_words( strip_tags($excerpt), 25, '&nbsp;&hellip;' );
+}
+add_filter( 'bp_get_group_description_excerpt', 'telabotanica_group_description_excerpt', 99 );

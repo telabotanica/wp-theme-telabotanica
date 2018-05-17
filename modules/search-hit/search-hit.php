9508f8c050a05601da209e7261eb80edae86baf6
Hans Lemuet <hans@etaminstudio.com>
2018-05-17 16:19:32 +0200

    search-hit: disable server-side rendering (taiga 894)

--- modules/search-hit/search-hit.php
+++ modules/search-hit/search-hit.php
@@ -1,12 +1,3 @@
 <?php function telabotanica_module_search_hit($data) {
-  global $pug;
-
-  $defaults = [
-  ];
-
-  $data = telabotanica_styleguide_data($defaults, $data);
-
-  echo $pug->render(__DIR__ . '/search-hit.pug', [
-    'data' => $data
-  ]);
+  // Ce module n'est pas sensé être rendu côté serveur.
 }

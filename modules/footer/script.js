dc2a06f2f3cc8d7712fc84d1e61df95e78944764
Hans Lemuet <hans@etaminstudio.com>
2018-06-06 15:54:05 +0200

    footer: add toggle for site map

--- modules/footer/script.js
+++ modules/footer/script.js
@@ -7,13 +7,23 @@ Tela.modules.footer = (function(){
 
   function module(selector){
     var $el     = $(selector),
+      $toggleNav,
+      $nav,
       $itemsMore;
 
     function init(){
+      $toggleNav = $el.find('.footer-nav-toggle button');
+      $nav = $el.find('.footer-nav');
       $itemsMore = $el.find('.menu-item-more');
+
+      $toggleNav.on('click', toggleNav);
 
       var iconArrowRight = iconTemplate({data: {icon: 'arrow-right'}});
       $itemsMore.append(iconArrowRight);
+    }
+
+    function toggleNav(){
+      $nav.toggle();
     }
 
     init();

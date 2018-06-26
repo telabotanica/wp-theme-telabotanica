6d64bce365485d990df476146a84ffcda73cd3d3
Hans Lemuet <hans@etaminstudio.com>
2018-06-26 18:09:59 +0200

    Proposer une actu: remove h1 from TinyMCE editor (taiga 691)

--- functions.php
+++ functions.php
@@ -271,3 +271,12 @@ add_role( 'tb_president', __('PrésidentTB', 'telabotanica' ),
     'upload_files' => true
   )
 );
+
+// Modify TinyMCE editor to remove H1.
+// from https://www.jowaltham.com/modify-tinymce-editor/
+function tiny_mce_remove_unused_formats($init) {
+	// Add block format elements you want to show in dropdown
+	$init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;Address=address;Pre=pre';
+	return $init;
+}
+add_filter('tiny_mce_before_init', 'tiny_mce_remove_unused_formats' );

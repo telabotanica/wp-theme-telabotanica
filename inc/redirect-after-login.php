1babaafce01dd2af8ca9eff53834941cc03d6edc
kstefanini <stefanini.k@gmail.com>
2019-10-25 11:14:37 +0200

    Fix warning when user is an error

--- inc/redirect-after-login.php
+++ inc/redirect-after-login.php
@@ -2,15 +2,18 @@
 /**
  * Redirects non-admins to the homepage after logging into the site, unless the
  * $redirect_to string is set
+ * Third param $user can be a WP_Error with login errors
  */
 function tb_login_redirect( $redirect_to, $request, $user ) {
 
   $url = $redirect_to;
 
   // Ex-telabotaniste ('deleted_tb_user' role user) can't login
-  if (is_array( $user->roles ) && in_array( 'deleted_tb_user', $user->roles)) {
-    wp_logout();
-    $url = site_url();
+  if (! is_wp_error($user)) {
+    if (is_array( $user->roles ) && in_array( 'deleted_tb_user', $user->roles)) {
+      wp_logout();
+      $url = site_url();
+    }
   }
 
   if (empty($redirect_to) || (admin_url() == $redirect_to)) {

103dad234845b996fd36912240d47f7f16e41d48
Killian STEFANINI <kstefanini@users.noreply.github.com>
2020-01-06 16:18:08 +0100

    Revert "ajout du nouveau paramètre standard=0 (#174)"
    This reverts commit 3f0efa217678df75fd4ce899cf6b15a8f595b300.

--- modules/block-dashboard-observations/block-dashboard-observations.php
+++ modules/block-dashboard-observations/block-dashboard-observations.php
@@ -2,7 +2,7 @@
   $defaults = [
     'title' => [],
     'modifiers' => ['block-dashboard-observations', 'transparent-content'],
-    'api_url' => 'https://api.tela-botanica.org/service:del:0.1/observations?navigation.depart=0&navigation.limite=5&masque.pninscritsseulement=1&masque.type=adeterminer&tri=date_transmission&ordre=desc&standard=0'
+    'api_url' => 'https://api.tela-botanica.org/service:del:0.1/observations?navigation.depart=0&navigation.limite=5&masque.pninscritsseulement=1&masque.type=adeterminer&tri=date_transmission&ordre=desc'
     // For local debugging:
     // 'api_url' => '/wp-content/themes/telabotanica/modules/block-dashboard-observations/test.json'
   ];

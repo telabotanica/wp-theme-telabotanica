9dff52cb1bffc712b756d3b62229162d95baf7fc
arthur-tela <arthur@tela-botanica.org>
2023-08-02 16:08:16 +0200

    reinstating_google_analytics

--- header.php
+++ header.php
@@ -12,6 +12,15 @@
   <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php endif; ?>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-RY61Q4VD36"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-RY61Q4VD36');
+  </script>
   <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1">
   <?php wp_head(); ?>
 </head>
